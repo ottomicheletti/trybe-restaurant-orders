d18b5f677bf75fdc6b66f5e4064f80f6c17bab04
ottomicheletti <michelettiotto@gmail.com>
2022-12-14 17:54:34 -0300

    feat(Req 01): :sparkles: Implements analyze_log function within analyze_log.py
    First the function checks if the file has the right extension. If not, it raises a FileNotFoundError with the message "Extensão inválida: 'file.ext'". After this test, the function opens the .csv file and executes the following operations: 1) put in a set all foods existing in the thid column of the file, 2) put in another set all working days, 3) filter all orders by Maria and returns the most commom, 4) filters all orders by arnaldo and returns how many hamburgers he had eaten, 5) filters all dishes eaten by Arnaldo and returns the difference between the all_foods set, 6) again filters for Arnaldo, but now for the days he attended and returning the difference between the restaurant working_days. After the last four operations, the function writes into a .txt file the returns of these lists/sets manipulation. The result can be found under data/mkt_campaign.txt.

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,54 @@
+import os
+import csv
+from statistics import mode
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    filename, ext = os.path.splitext(path_to_file)
+
+    if ext != '.csv':
+        raise FileNotFoundError(f"Extensão inválida: '{filename}{ext}'")
+
+    try:
+        with open(path_to_file, "r") as file:
+            orders = list(csv.reader(file))
+            all_foods = set([
+                order[1]
+                for order in orders
+            ])
+
+            working_days = set([
+                order[2]
+                for order in orders
+            ])
+
+            maria_most_ordered = mode([
+            order[1]
+            for order in orders
+            if order[0] == 'maria'
+            ])
+
+            hamburgers_eaten_by_arnaldo = len([
+                order[1]
+                for order in orders
+                if order[0] == 'arnaldo' and order[1] == 'hamburguer'
+            ])
+
+            dishes_joao_never_ordered = all_foods.difference(set([
+                order[1]
+                for order in orders
+                if order[0] == 'joao'
+            ]))
+
+            days_arnaldo_never_attended = working_days.difference(set([
+                order[2]
+                for order in orders
+                if order[0] == 'arnaldo'
+            ]))
+
+            lines = [maria_most_ordered, hamburgers_eaten_by_arnaldo, dishes_joao_never_ordered, days_arnaldo_never_attended]
+            with open('data/mkt_campaign.txt', "w") as txt:
+                for line in lines:
+                    txt.write(f'{line}\n')
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{filename}{ext}'")
