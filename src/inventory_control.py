61265976a50714380349cf1b0c5a88370ed5fbcd
ottomicheletti <michelettiotto@gmail.com>
2022-12-14 22:34:47 -0300

    feat(Bonus): :sparkles: Defines the get_available_dishes method and __available_ingredient and __available_dishes attributes in InventoryControl

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -6,7 +6,7 @@ class InventoryControl:
             'misto-quente': ['pao', 'queijo', 'presunto'],
             'coxinha': ['massa', 'frango'],
         }
-        self.__minimum_inventory = {
+        self.__minimum_stock = {
             'pao': 50,
             'carne': 50,
             'queijo': 100,
@@ -24,13 +24,26 @@ class InventoryControl:
             'massa': 0,
             'frango': 0,
         }
+        self.__available_ingredients = list()
+        self.__available_dishes = set()
 
     def add_new_order(self, _customer, order, _day):
         for item in self.__ingredients[order]:
-            if self.__total_ingredients[item] < self.__minimum_inventory[item]:
+            if self.__total_ingredients[item] < self.__minimum_stock[item]:
                 self.__total_ingredients[item] += 1
             else:
                 return False
 
     def get_quantities_to_buy(self):
         return self.__total_ingredients
+
+    def get_available_dishes(self):
+        for (key, value) in self.__total_ingredients.items():
+            if value < self.__minimum_stock[key]:
+                self.__available_ingredients.append(key)
+
+        for (key, value) in self.__ingredients.items():
+            if set(value).issubset(set(self.__available_ingredients)):
+                self.__available_dishes.add(key)
+
+        return self.__available_dishes
