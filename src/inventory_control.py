73ef87dc0b05c0b20157f1f48805c24f6fb3a255
ottomicheletti <michelettiotto@gmail.com>
2022-12-14 21:49:54 -0300

    feat(Bonus): :sparkles: Defines both add_new_order and get_quantities_to_buy methods in InventoryControl.

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,36 @@
 class InventoryControl:
-    INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
-    }
-    MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
-    }
-
     def __init__(self):
-        pass
+        self.__ingredients = {
+            'hamburguer': ['pao', 'carne', 'queijo'],
+            'pizza': ['massa', 'queijo', 'molho'],
+            'misto-quente': ['pao', 'queijo', 'presunto'],
+            'coxinha': ['massa', 'frango'],
+        }
+        self.__minimum_inventory = {
+            'pao': 50,
+            'carne': 50,
+            'queijo': 100,
+            'molho': 50,
+            'presunto': 50,
+            'massa': 50,
+            'frango': 50,
+        }
+        self.__total_ingredients = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
 
-    def add_new_order(self, customer, order, day):
-        pass
+    def add_new_order(self, _customer, order, _day):
+        for item in self.__ingredients[order]:
+            if self.__total_ingredients[item] < self.__minimum_inventory[item]:
+                self.__total_ingredients[item] += 1
+            else:
+                return False
 
     def get_quantities_to_buy(self):
-        pass
+        return self.__total_ingredients
